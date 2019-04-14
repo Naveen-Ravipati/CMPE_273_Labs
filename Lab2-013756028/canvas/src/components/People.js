56e5ac7e2647d074653c3ce988a9f0c165f01012
Naveen Ravipati <naveenravipati7gmail.com>
2019-04-14 03:51:27 -0700

    Pagination Completed for People

--- Lab2-013756028/canvas/src/components/People.js
+++ Lab2-013756028/canvas/src/components/People.js
@@ -2,89 +2,122 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class People extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state = {  
-            people : [],
+        this.state = {
+            people: [],
+            prev_count: 0,
+            next_count: 2,
         }
     }
 
-    new_function = ()=>{
+    new_function = () => {
         const data = {
             course_id: localStorage.getItem('course_id')
         }
         var token = localStorage.getItem("token");
-        axios.post('http://localhost:3001/people',data,{
-            headers: {"Authorization" : `Bearer ${token}`}})
-                .then((response) => {
+        axios.post('http://localhost:3001/people', data, {
+            headers: { "Authorization": `Bearer ${token}` }
+        })
+            .then((response) => {
                 //update the state with the response data
-                    this.setState({
-                    people : response.data
-                    })
+                this.setState({
+                    people: response.data
+                })
                 console.log(this.state.people)
             });
     }
 
-    drop_course = async(val)=>{
+    drop_course = async (val) => {
         const data = {
-            course_id:localStorage.getItem('course_id'),
-            student_id:val.student_id
+            course_id: localStorage.getItem('course_id'),
+            student_id: val.student_id
         }
         console.log(data.student_id)
         var token = localStorage.getItem("token");
-        await axios.post('http://localhost:3001/drop_course',data,{
-            headers: {"Authorization" : `Bearer ${token}`}})
-        .then((response) => {
-        if(response.status == 200){
-            alert('Course Dropped')
-        }
-    });
-    
-    this.new_function()
+        await axios.post('http://localhost:3001/drop_course', data, {
+            headers: { "Authorization": `Bearer ${token}` }
+        })
+            .then((response) => {
+                if (response.status == 200) {
+                    alert('Course Dropped')
+                }
+            });
+
+        this.new_function()
     }
 
 
-    componentDidMount=async()=>{
-        alert('here')
+    componentDidMount = async () => {
+        // alert('here')
         const data = {
             course_id: localStorage.getItem('course_id')
         }
         var token = localStorage.getItem("token");
-        await axios.post('http://localhost:3001/people',data,{
-            headers: {"Authorization" : `Bearer ${token}`}})
-                .then((response) => {
+        await axios.post('http://localhost:3001/people', data, {
+            headers: { "Authorization": `Bearer ${token}` }
+        })
+            .then((response) => {
                 //update the state with the response data
-                    this.setState({
-                    people : response.data
-                    })
+                this.setState({
+                    people: response.data
+                })
                 console.log(this.state.people)
             });
     }
 
+    next_page = (e) => {
+        if (this.state.next_count < this.state.people.length) {
+            this.setState({
+                next_count: this.state.next_count + 2,
+                prev_count: this.state.prev_count + 2
+            })
+        }
+        if (this.state.next_count >= this.state.people.length) {
 
-    render(){
+            alert("No more content")
+        }
+    }
+
+    previous_page = (e) => {
+        if (this.state.prev_count > 1) {
+            this.setState({
+                next_count: this.state.next_count - 2,
+                prev_count: this.state.prev_count - 2
+            })
+        }
+        if (this.state.prev_count <= 0) {
+            alert("No previous content")
+        }
+    }
+
+
+    render() {
         //iterate over books to create a table row
+        if(this.state.people.length>0){
         let visibility = ''
         let student_or_faculty = localStorage.getItem('student_or_faculty')
-        if(student_or_faculty == 'student'){
-            visibility = 'hidden' 
+        if (student_or_faculty == 'student') {
+            visibility = 'hidden'
         }
-        else{
+        else {
             visibility = 'visible'
         }
-        let details = this.state.people.map(student => {
-            return(
+        var details = this.state.people.map((student,idx) => {
+            if(idx>=this.state.prev_count && idx<this.state.next_count){
+            return (
                 <tr>
                     <td>{student.student_id}</td>
                     <td>{student.name}</td>
                     <td>Student</td>
-                    <td  style = {{visibility:visibility}}><button onClick= {this.drop_course.bind(this,student)}>Drop</button></td>
+                    <td style={{ visibility: visibility }}><button onClick={this.drop_course.bind(this, student)}>Drop</button></td>
                 </tr>
             )
+            }
         })
-        //if not logged in go to login page
+    }
 
-        return(
+        return (
             <div>
                 <div class="container">
                     <h2>People</h2>
@@ -99,10 +132,12 @@ class People extends Component {
                         <tbody>
                             {/*Display the Tbale row based on data recieved*/}
                             {details}
+                            <button onClick={this.previous_page} class="btn btn-primary" type="button" style={{ marginLeft: "100px" }}>Previous</button>
+                            <button onClick={this.next_page} class="btn btn-primary" type="button" style={{ marginLeft: "350px" }}>Next</button>
                         </tbody>
                     </table>
-                </div> 
-            </div> 
+                </div>
+            </div>
         )
     }
 
